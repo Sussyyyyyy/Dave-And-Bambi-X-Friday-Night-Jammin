6b97abf67e32f8c918ce69796c2a2631021abf0e
ValenciaBall <desamcamacho@gmail.com>
2022-08-18 13:26:21 +0200

    redacted y el yo y los sin consola

---
+++ assets/preload/data/yoylossinconsola/window.lua
@@ -0,0 +1,19 @@
+function opponentNoteHit()
+  windowShake()
+end
+
+function windowShake()
+  windowX = getPropertyFromClass('openfl.Lib', 'application.window.x')
+  windowY = getPropertyFromClass('openfl.Lib', 'application.window.y')
+  setPropertyFromClass('openfl.Lib','application.window.x',windowX + math.random(-10,10))
+  setPropertyFromClass('openfl.Lib','application.window.y',windowY + math.random(-5,5))
+end
+
+function onMoveCamera(focus)
+	if focus == 'boyfriend' then
+		-- called when the camera focus on boyfriend
+	elseif focus == 'dad' then
+		setProperty('camFollowPos.y',getProperty('camFollowPos.y') + (math.sin(currentBeat) * 0.6))
+	end
+	setPropertyFromClass("openfl.Lib", "application.window.title", "STUMBLE FUNKIN SONG 1 YO Y LOS SIN CONSOLA HARDEST SONG")
+end
